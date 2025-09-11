7959981f210c82a0a1c7d3e941711c47a187d0e5
Gleybson Rodrigues <145180786+GleybsonRodrigues03@users.noreply.github.com>
2025-09-10 21:11:29 -0300

    Add files via upload

---
+++ new10.09/server.js
@@ -0,0 +1,47 @@
+// let a = 7
+// let b = 5
+// // let nome = 'Kami'
+// // let idade = true
+// let media = (a+b) / 2
+
+// console.log("A média é: ", media)
+
+
+//array
+// let arrayNome = ['Filho', 'maria', 'Kami']
+// let arrayNumero = [1, 2, 3, 4, 5]
+// console.log('A', arrayNome[1], "tem", arrayNumero[3], "anos.")
+
+
+//object
+// let objtNome = {
+//     nome: 'gleybson',
+//     sobreNome: 'rodrigues',
+//     idade: true
+// }
+// console.log(objtNome.nome, objtNome.sobreNome)
+
+
+// //if
+// let numeroUm = 2
+// let numeroDois = 2
+
+// if (numeroUm > numeroDois) {
+//     console.log("numeroUM é maior.")
+// } else {
+//     console.log("numeroDois é maior")
+// }
+
+// switch (numeroUm) {
+//     case 0:
+//         console.log("valor 0")
+//         break;
+//     case 1: 
+//         console.log("valor 1")
+//         break;
+//     case 2:
+//         console.log("valor 2")
+//         break;
+//     default:
+//         console.log("nenhum valor entrou no case")
+// }
