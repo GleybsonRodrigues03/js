9a9d2ea9d900c0c4bac8e05b5f214c3ee5096933
Gleybson Rodrigues <145180786+GleybsonRodrigues03@users.noreply.github.com>
2025-09-16 20:50:20 -0300

    Exercícios usando Function

---
+++ aula16.09/script.js
@@ -0,0 +1,105 @@
+// let valor1 = 2
+// let valor2 = 3
+// let resultado = Somar(valor1, valor2)
+
+// function Somar() {
+//     return valor1+ valor2
+// }
+// console.log(resultado)
+
+// let id = (prompt("Entre com sua idade"));
+
+// let ano1 = 2025;
+
+// let Result = IdadeAtual(id,ano1);
+// let Resultado2 = Idade(Result, ano1)
+
+// function IdadeAtual(id,ano1) {
+//     return ano1 - id;
+// }
+
+// alert("O seu ano de Nascimento é: " + Result + "e sua idade é: " + Resultado2);
+
+// function Idade(Result, ano1){
+//     return ano1-Result
+// }
+
+
+
+// let valor = parseFloat(prompt("Entre com o valor"))
+// const DESC = 0.15
+// let vr = AplicarDesconto()
+// function AplicarDesconto() {
+//     return valor-(valor * DESC)
+// }
+// console.log(vr)
+
+
+
+// let valor1 = parseFloat(prompt("Digite o primeiro valor: "))
+// let valor2 = parseFloat(prompt("Digite o segundo valor: "))
+// let div = Divisao()
+
+// function Divisao(){
+//     return valor1/valor2
+// }
+// console.log(div)
+
+
+
+// let distancia = parseFloat(prompt("Quantos quilômetros: "))
+// let combustivel = parseFloat(prompt("Quantos de combustivel: "))
+// let media1 = media()
+
+// function media() {
+//     return combustivel / distancia
+// }
+
+// console.log("final: " + media1)
+
+
+
+// let produto = parseFloat(prompt("Digite o valor total produto: "))
+// let desconto = parseFloat(prompt("Digite o valor com desconto: "))
+// let valorComDesconto = vcd()
+
+// function vcd() {
+//     return 100-((desconto*100)/produto)   
+// }
+// console.log(valorComDesconto)
+
+
+
+
+// let n1 = parseFloat(prompt("Digite primeiro número: "))
+// let n2 = parseFloat(prompt("Digite segundo número: "))
+// let Somar = soma()
+// let Multi = multiplicacao()
+// let Divi = divisao()
+// let Sub = subtracao()
+
+// function soma() {
+//     return n1+n2
+// }
+
+// function multiplicacao() {
+//     return n1*n2
+// }
+
+// function divisao() {
+//     return n1/n2
+// }
+
+// function subtracao() {
+//     return n1-n2
+// }
+
+// console.log("Valor soma: " + soma())
+// console.log("Valor multiplicação: " + multiplicacao())
+// console.log("Valor divisão: " + divisao())
+// console.log("Valor subtração: " + subtracao())
+
+
+
+
+dddd
