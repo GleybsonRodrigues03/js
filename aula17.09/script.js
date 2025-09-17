1b283d8fc9fbdcaccdebd8273830da27f1338edd
Gleybson Rodrigues <145180786+GleybsonRodrigues03@users.noreply.github.com>
2025-09-17 20:57:26 -0300

    Exercicios

---
+++ aula17.09/script.js
@@ -0,0 +1,57 @@
+// FOR
+
+// for (let index = 0; index < 5; index++) {
+//     console.log("o index é ", index)
+// }
+
+
+
+//ARRAY
+
+// let array = ['maria', 'joao', 'lucas']
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log('O elemento é ', element)
+// }
+
+
+// let array = [10, 20, 30, 40, 50]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index]
+//     if (array[index] == 40)
+//         console.log(element)
+// }
+
+
+
+//WHILE
+
+// let condition = true
+// while (condition) {
+//     console.log("entrou no wwhile")
+// }
+
+
+
+//////
+// let arrayFrutas = ["Maça", "Banana", "Morango" , "Uva"]
+// console.log(arrayFrutas[1])
+
+
+
+//////
+// let objLivro = {
+//     titulo: "filhodokami01",
+//     editora: "brasil",
+//     classificacao: "comédia"
+// }
+// console.log(objLivro)
+
+
+
+///////
+// let idade = parseFloat(prompt("Digite sua idade: "))
+// if (idade <= 17)
+//     console.log("Você é menor de idade")
+// else
+//     console.log("Você é maior de idade")
