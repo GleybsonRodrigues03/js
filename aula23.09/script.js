930647f5d2113f4c843c645187509a4820471ff3
Gleybson Rodrigues <145180786+GleybsonRodrigues03@users.noreply.github.com>
2025-09-23 21:00:15 -0300

    Exercícios com DOM

---
+++ aula23.09/script.js
@@ -0,0 +1,30 @@
+// function Media() {
+//     let n1= Number(document.querySelector("#n1").value)
+//     let n2= Number(document.querySelector("#n2").value)
+//     let n3= Number(document.querySelector("#n3").value)
+
+//     let media = (n1+n2+n3)/3
+
+    
+//     if (media >= 7) {
+//         document.querySelector("#resultado").textContent=`${media} aprovado`
+
+//     } else if (media >=5) {
+//         document.querySelector("#resultado").textContent=`${media} reprovado`
+
+//     } else {
+//         document.querySelector("#resultado").textContent=`${media} aluno reprovado`
+
+//     }
+
+// }
+
+// function Salario() {
+//     let salario = Number(document.querySelector("#salario").value)
+//     valoraumento = 0
+//     document.querySelector("#resultado20").textContent=`${salario + (salario*0.2)} com aumento de 20%`
+//     document.querySelector("#resultado15").textContent=`${salario + (salario*0.15) } com aumento de 15%`
+//     document.querySelector("#resultado10").textContent=`${salario + (salario*0.1) } com aumento de 10%`
+//     document.querySelector("#resultado05").textContent=`${salario + (salario*0.05)} com aumento de 5%`
+
+// }
